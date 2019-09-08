5def3a13a3b528da69b87c0620c18f55df4cb750
neloreck <Neloreck@gmail.com>
2019-09-09 01:01:39 +0300

    Codestyle.

--- src/application/Application.ts
+++ src/application/Application.ts
@@ -4,6 +4,8 @@ import { hydrate, render } from "react-dom";
 // Lib.
 import { APPLICATION_ROOT } from "@Build/build_constants";
 import { EntryPoint } from "@Lib/decorators";
+
+// Data.
 import { applicationConfig } from "@Main/data/configs/ApplicationConfig";
 
 // View.
@@ -18,40 +20,28 @@ export class Application {
       this.renderDevmode();
     } else {
       this.render();
-    }
+    } // Else if SSR.
   }
 
   /**
    * Common render.
    */
   public static render(): void {
-
-    render(
-      createElement(Root),
-      document.getElementById(APPLICATION_ROOT)
-    );
+    render(createElement(Root),document.getElementById(APPLICATION_ROOT));
   }
 
   /**
    * Render with StrictMode for debug.
    */
   public static renderDevmode(): void {
-
-    render(
-      createElement(StrictMode, {}, createElement(Root)),
-      document.getElementById(APPLICATION_ROOT)
-    );
+    render(createElement(StrictMode, {}, createElement(Root)), document.getElementById(APPLICATION_ROOT));
   }
 
   /**
    * Hydrate DOM after SSR pre-render.
    */
   public static hydrate(): void {
-
-    hydrate(
-      createElement(Root),
-      document.getElementById(APPLICATION_ROOT)
-    );
+    hydrate(createElement(Root), document.getElementById(APPLICATION_ROOT));
   }
 
 }
